fd029be22352452923b1923f395f90556bd32290
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2023-06-14 18:33:28 +0200

    first commit

---
+++ Scenes/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var player_sprite = $player_sprite
+
+func _physics_process(delta):
+
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	else:
+		if(velocity.x == 0):
+			player_sprite.animation = "idle"
+		else:
+			player_sprite.animation = "walk"
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		
+		if (velocity.x < 0):
+			player_sprite.flip_h = true
+		if (velocity.x > 0):
+			player_sprite.flip_h = false
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
